b531efc55645f91285f86e99850ab88c49406b9d
Cole Gawin <colegawin@gmail.com>
2021-06-29 15:30:29 -0500

    feat: switch to plausible analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,12 +13,7 @@ export default class MyDocument extends NextDocument<{ css: string }> {
     return (
       <Html lang={"en"}>
         <Head>
-          <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PT2X84T');
-`}</script>
+          <script defer data-domain="projects.colegaw.in" src="https://plausible.io/js/plausible.js"></script>
 
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -38,15 +33,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
         </Head>
         <body>
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-PT2X84T"
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
-
           <Main />
           <NextScript />
         </body>
